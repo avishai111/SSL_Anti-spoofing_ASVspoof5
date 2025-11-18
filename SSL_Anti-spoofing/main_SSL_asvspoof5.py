6d7ae7b4529423c1c84ff51834f5cc3bab89d6a7
Avishai Weizman <Avishai Weizman>
2025-11-18 20:25:56 +0000

    adding files

--- SSL_Anti-spoofing/main_SSL_asvspoof5.py
+++ SSL_Anti-spoofing/main_SSL_asvspoof5.py
@@ -20,6 +20,179 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset
 from tqdm import tqdm
 import librosa
+from model import *
+
+
+class WrappedModel(nn.Module):
+    def __init__(self, args,device):
+        super().__init__()
+        self.device = device
+        
+        # AASIST parameters
+        filts = [128, [1, 32], [32, 32], [32, 64], [64, 64]]
+        gat_dims = [64, 32]
+        pool_ratios = [0.5, 0.5, 0.5, 0.5]
+        temperatures =  [2.0, 2.0, 100.0, 100.0]
+
+
+        ####
+        # create network wav2vec 2.0
+        ####
+        self.ssl_model = SSLModel(self.device)
+        self.LL = nn.Linear(self.ssl_model.out_dim, 128)
+
+        self.first_bn = nn.BatchNorm2d(num_features=1)
+        self.first_bn1 = nn.BatchNorm2d(num_features=64)
+        self.drop = nn.Dropout(0.5, inplace=True)
+        self.drop_way = nn.Dropout(0.2, inplace=True)
+        self.selu = nn.SELU(inplace=True)
+
+        # RawNet2 encoder
+        self.encoder = nn.Sequential(
+            nn.Sequential(Residual_block(nb_filts=filts[1], first=True)),
+            nn.Sequential(Residual_block(nb_filts=filts[2])),
+            nn.Sequential(Residual_block(nb_filts=filts[3])),
+            nn.Sequential(Residual_block(nb_filts=filts[4])),
+            nn.Sequential(Residual_block(nb_filts=filts[4])),
+            nn.Sequential(Residual_block(nb_filts=filts[4])))
+
+        self.attention = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=(1,1)),
+            nn.SELU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 64, kernel_size=(1,1)),
+            
+        )
+        # position encoding
+        self.pos_S = nn.Parameter(torch.randn(1, 42, filts[-1][-1]))
+        
+        self.master1 = nn.Parameter(torch.randn(1, 1, gat_dims[0]))
+        self.master2 = nn.Parameter(torch.randn(1, 1, gat_dims[0]))
+        
+        # Graph module
+        self.GAT_layer_S = GraphAttentionLayer(filts[-1][-1],
+                                               gat_dims[0],
+                                               temperature=temperatures[0])
+        self.GAT_layer_T = GraphAttentionLayer(filts[-1][-1],
+                                               gat_dims[0],
+                                               temperature=temperatures[1])
+        # HS-GAL layer 
+        self.HtrgGAT_layer_ST11 = HtrgGraphAttentionLayer(
+            gat_dims[0], gat_dims[1], temperature=temperatures[2])
+        self.HtrgGAT_layer_ST12 = HtrgGraphAttentionLayer(
+            gat_dims[1], gat_dims[1], temperature=temperatures[2])
+        self.HtrgGAT_layer_ST21 = HtrgGraphAttentionLayer(
+            gat_dims[0], gat_dims[1], temperature=temperatures[2])
+        self.HtrgGAT_layer_ST22 = HtrgGraphAttentionLayer(
+            gat_dims[1], gat_dims[1], temperature=temperatures[2])
+
+        # Graph pooling layers
+        self.pool_S = GraphPool(pool_ratios[0], gat_dims[0], 0.3)
+        self.pool_T = GraphPool(pool_ratios[1], gat_dims[0], 0.3)
+        self.pool_hS1 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
+        self.pool_hT1 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
+
+        self.pool_hS2 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
+        self.pool_hT2 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
+        
+        self.out_layer = nn.Linear(5 * gat_dims[1], 2)
+
+    def forward(self, x, return_feat = True):
+        #-------pre-trained Wav2vec model fine tunning ------------------------##
+        x_ssl_feat = self.ssl_model.extract_feat(x.squeeze(-1))
+        x = self.LL(x_ssl_feat) #(bs,frame_number,feat_out_dim)
+        
+        # post-processing on front-end features
+        x = x.transpose(1, 2)   #(bs,feat_out_dim,frame_number)
+        x = x.unsqueeze(dim=1) # add channel 
+        x = F.max_pool2d(x, (3, 3))
+        x = self.first_bn(x)
+        x = self.selu(x)
+
+        # RawNet2-based encoder
+        x = self.encoder(x)
+        x = self.first_bn1(x)
+        x = self.selu(x)
+        
+        w = self.attention(x)
+        
+        #------------SA for spectral feature-------------#
+        w1 = F.softmax(w,dim=-1)
+        m = torch.sum(x * w1, dim=-1)
+        e_S = m.transpose(1, 2) + self.pos_S 
+        
+        # graph module layer
+        gat_S = self.GAT_layer_S(e_S)
+        out_S = self.pool_S(gat_S)  # (#bs, #node, #dim)
+        
+        #------------SA for temporal feature-------------#
+        w2 = F.softmax(w,dim=-2)
+        m1 = torch.sum(x * w2, dim=-2)
+     
+        e_T = m1.transpose(1, 2)
+       
+        # graph module layer
+        gat_T = self.GAT_layer_T(e_T)
+        out_T = self.pool_T(gat_T)
+        
+        # learnable master node
+        master1 = self.master1.expand(x.size(0), -1, -1)
+        master2 = self.master2.expand(x.size(0), -1, -1)
+
+        # inference 1
+        out_T1, out_S1, master1 = self.HtrgGAT_layer_ST11(
+            out_T, out_S, master=self.master1)
+
+        out_S1 = self.pool_hS1(out_S1)
+        out_T1 = self.pool_hT1(out_T1)
+
+        out_T_aug, out_S_aug, master_aug = self.HtrgGAT_layer_ST12(
+            out_T1, out_S1, master=master1)
+        out_T1 = out_T1 + out_T_aug
+        out_S1 = out_S1 + out_S_aug
+        master1 = master1 + master_aug
+
+        # inference 2
+        out_T2, out_S2, master2 = self.HtrgGAT_layer_ST21(
+            out_T, out_S, master=self.master2)
+        out_S2 = self.pool_hS2(out_S2)
+        out_T2 = self.pool_hT2(out_T2)
+
+        out_T_aug, out_S_aug, master_aug = self.HtrgGAT_layer_ST22(
+            out_T2, out_S2, master=master2)
+        out_T2 = out_T2 + out_T_aug
+        out_S2 = out_S2 + out_S_aug
+        master2 = master2 + master_aug
+
+        out_T1 = self.drop_way(out_T1)
+        out_T2 = self.drop_way(out_T2)
+        out_S1 = self.drop_way(out_S1)
+        out_S2 = self.drop_way(out_S2)
+        master1 = self.drop_way(master1)
+        master2 = self.drop_way(master2)
+
+        out_T = torch.max(out_T1, out_T2)
+        out_S = torch.max(out_S1, out_S2)
+        master = torch.max(master1, master2)
+
+        # Readout operation
+        T_max, _ = torch.max(torch.abs(out_T), dim=1)
+        T_avg = torch.mean(out_T, dim=1)
+
+        S_max, _ = torch.max(torch.abs(out_S), dim=1)
+        S_avg = torch.mean(out_S, dim=1)
+        
+        last_hidden = torch.cat(
+            [T_max, T_avg, S_max, S_avg, master.squeeze(1)], dim=1)
+        
+        last_hidden = self.drop(last_hidden)
+        output = self.out_layer(last_hidden)
+        
+        if return_feat:
+            return out, x_ssl_feat
+
+        return output
+
 
 def genSpoof_list(dir_meta,is_train=False,is_eval=False):
     
@@ -61,7 +234,7 @@ def evaluate_accuracy(dev_loader, model, device):
             num_total += batch_size
             batch_x = batch_x.to(device)
             batch_y = batch_y.view(-1).type(torch.int64).to(device)
-            batch_out = model(batch_x)
+            batch_out, x_ssl_feat = model(batch_x)
             
             batch_loss = criterion(batch_out, batch_y)
             val_loss += (batch_loss.item() * batch_size)
@@ -75,15 +248,15 @@ def produce_evaluation_file(dataset, model, device, save_path):
     # אפשר לשנות את batch_size ו-num_workers לפי הזיכרון שלך
     data_loader = DataLoader(
         dataset,
-        batch_size=32,
+        batch_size=1,
         shuffle=False,
         drop_last=False,
-        num_workers=4,
+        num_workers=0,
     )
 
     model.eval()
     pbar = tqdm(data_loader, total=len(data_loader))
-
+    ssl_feats = {}
 
     with torch.no_grad(), open(save_path, 'w') as fh:
         for batch_x, utt_id in pbar:
@@ -91,7 +264,7 @@ def produce_evaluation_file(dataset, model, device, save_path):
             
             batch_x = batch_x.to(device, non_blocking=True)
 
-            batch_out = model(batch_x)
+            batch_out, x_ssl_feat = model(batch_x)
             batch_score = batch_out[:, 1].detach().cpu().numpy().ravel()
 
             pbar.set_description(f"Processing: {utt_id[0]}")
@@ -100,7 +273,13 @@ def produce_evaluation_file(dataset, model, device, save_path):
             for f, cm in zip(utt_id, batch_score):
                 fh.write(f"{f} {cm}\n")
 
+            x_np = x_ssl_feat.detach().cpu().numpy() 
+            for i, f in enumerate(utt_id):
+                ssl_feats[f] = x_np[i] 
+
     print(f"Scores saved to {save_path}")
+    np.savez("all_ssl_features.npz", **ssl_feats)
+    print("Saved embeddings to all_ssl_features.npz")
 
 
 def train_epoch(train_loader, model, lr,optim, device):
@@ -121,7 +300,7 @@ def train_epoch(train_loader, model, lr,optim, device):
         
         batch_x = batch_x.to(device)
         batch_y = batch_y.view(-1).type(torch.int64).to(device)
-        batch_out = model(batch_x)
+        batch_out, xfeat_ssl = model(batch_x)
         
         batch_loss = criterion(batch_out, batch_y)
         
@@ -278,6 +457,7 @@ def run_asvspoof2021_baseline(
 
     if args.model_path:
         model.load_state_dict(torch.load(args.model_path, map_location=device))
+        model = WrappedModel(model)
         print('Model loaded : {}'.format(args.model_path))
 
     if args.eval:
